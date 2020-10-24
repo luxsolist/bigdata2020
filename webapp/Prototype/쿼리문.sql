46ad8cbe7d7927392e8a7a403e327436c722d989
ksh93 <49471740+kxxsh93@users.noreply.github.com>
2020-10-24 14:24:57 +0900

    Update 쿼리문.sql
    Analysis_result Table 추가

--- webapp/Prototype/쿼리문.sql
+++ webapp/Prototype/쿼리문.sql
@@ -1,7 +1,3 @@
-CREATE DATABASE Tourlist;
-
-USE Tourlist;
-
 CREATE TABLE TOURLIST_SITE(
 	tour_id INT NOT NULL,
 	title VARCHAR(255) NOT NULL,
@@ -52,6 +48,14 @@ CREATE TABLE TOURLIST_USER(
 	PRIMARY KEY(user_id) 
 )ENGINE = InnoDB;
 
+CREATE TABLE ANALYSIS_RESULT(
+	tour_id INT NOT NULL,
+	analyzed_at VARCHAR(31) NOT NULL,
+	readcount_score FLOAT,
+	congestion_score FLOAT,
+	sentiment_score FLOAT,
+	corona_risk FLOAT
+)ENGINE = InnoDB;
 
 LOAD DATA LOCAL INFILE 'C:\\bigdata2020\\data\\tour_data_tab.csv'
 INTO TABLE tourlist_site
